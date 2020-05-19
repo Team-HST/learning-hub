a47856138947d90feba8e11ee86023af4f01196b
dudgns0612 <dudgns0612@naver.com>
2020-05-19 21:57:36 +0900

    [FE] vuex, axios 기본설정

---
+++ learning-hub-fe/src/lib/axios/http.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+/**
+ * @description axios inteceptor reqeust 처리
+ */
+axios.interceptors.request.use((config) => {
+  console.log(config);
+}, (error) => {
+  return Promise.reject(error);
+});
+
+/**
+ * @description axios interceptor response 처리
+ */
+axios.interceptors.response.use((response) => {
+    return response;
+  }, (error) => {
+    // 공통 에러 처리
+    console.log(error);
+  }
+);
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete
+};
