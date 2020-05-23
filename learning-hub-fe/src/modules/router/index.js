8cd56b374d36a84fc6ccce11db33305937df3b3b
dudgns0612 <dudgns0612@naver.com>
2020-05-23 15:45:15 +0900

    [#1] F/E tovo templete 로그인, 회원가입 router 적용

--- learning-hub-fe/src/modules/router/index.js
+++ learning-hub-fe/src/modules/router/index.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HolloWorld from '@/components/HelloWorld'
 import Navbar from '@/components/navbar'
+import About from '@/components/about'
+import Home from '@/page/home'
+import SignIn from '@/page/sign_in'
+import SignUp from '@/page/sign_up'
 
-Vue.component('Navbar', Navbar)
-Vue.component('HolloWorld', HolloWorld)
+Vue.component('Navbar', Navbar) // 상단 메뉴 컴포넌트
+Vue.component('About', About)
+
+Vue.component('Home', Home)
+Vue.component('SignIn', SignIn)
+Vue.component('SingUp', SignUp)
 Vue.use(Router)
 
 export default new Router({
@@ -13,11 +20,25 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home_one',
+      name: 'home',
       components: {
-        default: HolloWorld,
+        default: Home,
         navbar: Navbar,
       }
     },
+    {
+      path: '/login',
+      name: 'login',
+      components: {
+        default: SignIn
+      }
+    },
+    {
+      path: '/join',
+      name: 'register',
+      components: {
+        default: SignUp
+      }
+    }
   ]
 })
---
+++ learning-hub-fe/src/page/home.vue
@@ -0,0 +1,51 @@
+<template>
+ <div id="main_div">
+
+   <!-- Home Section start-->
+   <section id="home" class="home">
+     <div class="home-decor">
+       <div class="home-circle1"><img :src='"../assets/images/main-banner3.png"' alt=""></div>
+       <div class="home-circle2"><img :src='"../assets/images/main-banner12.png"' alt=""></div>
+       <div class="home-circle3"><img :src='"../assets/images/main-banner1.png"' alt=""></div>
+     </div>
+     <div class="container">
+       <div class="row">
+         <div class="col-md-5">
+           <div class="home-contain">
+             <div>
+               <img :src='"../assets/images/logoimage.png"' alt="caption-img" class="caption-img">
+               <h4>For connecting generations</h4>
+               <h1>L<span class="f-bold">e</span>a<span class="f-bold">n</span>i<span class="f-bold">n</span>g
+                 <span class="f-bold f-color">hub</span>
+               </h1>
+               <p>Enhance your business ideas with Powerful, Responsive, Elegant TOVO Theme.</p>
+               <a href="#"><img :src='"../assets/images/appstore.png"' alt="appstore" class="store"></a>
+               <a href="#"><img class="ml-10 store" :src='"../assets/images/play-store.png"' alt="play-store"></a>
+             </div>
+           </div>
+         </div>
+         <div class="col-sm-6 offset-md-1">
+           <div class="home-right">
+             <!-- Tile js start -->
+             <Tile :image = '"mobile.png"'/>
+             <!-- Tile js end -->
+           </div>
+         </div>
+       </div>
+     </div>
+   </section>
+   <!--Home Section End -->
+
+   <About />
+
+  </div>  
+</template>
+  
+<script>
+  export default {
+  }
+</script>
+
+<style>
+
+</style>
